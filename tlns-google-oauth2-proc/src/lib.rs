2aebdbb2e6a7741aa40deddeb0af01ca4d1f1227
timelesnesses <timelessnesses@timelessnesses.me>
2024-06-30 17:07:59 +0700

    augh

--- tlns-google-oauth2-proc/src/lib.rs
+++ tlns-google-oauth2-proc/src/lib.rs
@@ -1,5 +1,6 @@
 /// # tlns-google-oauth2-proc
-/// This crate contains [`generate_scopes_enums`]
+/// This crate contains [`generate_scopes_enums`] that will generate a bunch of enum scopes with full scope conversion.
+/// This is intended to be used in [`tlns-google-oauth2`] but you can make changes to it.
 
 use std::collections::HashMap;
 
